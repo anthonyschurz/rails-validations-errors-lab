46a9c7cbcf2d60221b8ec39dae237873b82abbee
anthonyschurz <anthony.schurz@gmail.com>
2016-06-16 08:25:33 -0700

    morning commit

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,11 +1,33 @@
 class Owner < ActiveRecord::Base
-  # TODO: add validations
+
+  validates :first_name, presence: true, length: {maximum: 255}
+  validates :last_name, presence: true, length: {maximum: 255}
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, length: {maximum: 255}
 
   before_save :normalize_phone_number
 
-  # removes leading 1 and the characters (, ), -, .
+  before_save :normalize_phone_number
+
+  validates :first_name,
+            presence:   true,
+            length:   { maximum: 255 }
+
+  validates :last_name,
+            presence:   true,
+            length:   { maximum: 255 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence:   true,
+            format:     { with: VALID_EMAIL_REGEX },
+            uniqueness: true,
+            length:     { maximum: 255 }
+
   def normalize_phone_number
-    # stretch
+    if phone.present?
+      phone.gsub!(/^1/, "")
+      phone.gsub!(/[()-.]/,"")
+    end
   end
 
 end
